a22eeaa44490c220e4771e703fabee09c050edc9
tayebhakkar <tayebhakkar@yahoo.fr>
2021-05-23 19:38:07 +0200

    F A C T O R I S é

--- Petits_Chevaux/src/controleurs/btnListener.java
+++ Petits_Chevaux/src/controleurs/btnListener.java
@@ -27,107 +27,9 @@ public class btnListener implements ActionListener{
 	
 
 	public void actionPerformed(ActionEvent e) {
-		
+		//Simple game event
 		if(e.getSource()==window.getSimple()) {
-			window.getGameType().setVisible(false);
-			window.getDif().setVisible(true);
-			window.getButtonSimple().setVisible(true);
-			window.getC().add(window.getDif(),BorderLayout.CENTER);
-			window.getC().add(window.getButtonSimple(),BorderLayout.SOUTH);
-		}
-		
-		if(e.getSource()==window.getPerso()) {
-			game=null;
-			gboard.setNbHole(0);
-			gboard.setNbRiver(0);
-			gboard.setNbHedge(0);
-			
-			window.getGameType().setVisible(false);
-			window.getC().add(window.getSizePanel(),BorderLayout.NORTH);
-			sizeBoard=Integer.parseInt(window.getSelectBox().getSelectedItem().toString());
-			window.getSizePanel().setVisible(true);
-			window.getC().add(window.getButtonPerso(),BorderLayout.SOUTH);
-			window.getButtonPerso().setVisible(true);	
-		}
-		
-		if(e.getSource()==window.getBoxValidate()) {
-			sizeBoard=Integer.parseInt(window.getSelectBox().getSelectedItem().toString());
-			initGame();
-			window.getC().add(window.getCenter(),BorderLayout.CENTER);
-			window.getCenter().setVisible(true);
-			window.getPlayPerso().setEnabled(true);
-			gboard.addListener(game);
-		}
-		
-		if(e.getSource()==window.getPlayPerso()) {
-			window.getButtonPerso().setVisible(false);
-			
-			window.getSizePanel().setVisible(false);
-			
-			window.getC().add(window.getUp(),BorderLayout.NORTH);
-			window.getC().add(window.getCenter(),BorderLayout.CENTER);
-			window.getC().add(window.getDown(),BorderLayout.SOUTH);
-			
-			window.getUp().setVisible(true);
-			window.getCenter().setVisible(true);
-			window.getDown().setVisible(true);
-		
-			gboard.removeListener();
-		}
-		
-		if(e.getSource()==window.getRoll()){
-			game.getDice().rouler();
-			int value=game.getDice().getValue();
-			gboard.getCells().get((int)(game.getBoard().getCols()/2)).get(3).setLabel("  "+value+"  ");
-			play(value);
-		}
-		
-		if(e.getSource()==window.getStep()) {
-			int value=window.getStepValue();
-			window.getStep().setText("");
-			gboard.getCells().get((int)(game.getBoard().getCols()/2)).get(3).setLabel("  "+value+"  ");
-			play(value);
-		}
-		
-		if(e.getSource()==window.getPlay()) {
-			// Voir si on change
-			window.getButtonSimple().setVisible(false);
-			
-			window.getDif().setVisible(false);
-			initGame();
-			
-			window.getC().add(window.getUp(),BorderLayout.NORTH);
-			window.getC().add(window.getCenter(),BorderLayout.CENTER);
-			window.getC().add(window.getDown(),BorderLayout.SOUTH);
-			window.getUp().setVisible(true);
-			window.getCenter().setVisible(true);
-			window.getDown().setVisible(true);
-		
-		}
-		
-		if(e.getSource()==window.getRetourMenu()) {
-			window.getButtonSimple().setVisible(false);
-			window.getDif().setVisible(false);
-			window.getPlay().setEnabled(false);
-			window.getGameType().setVisible(true);
-			
-		}
-		
-		if(e.getSource()==window.getRetourMenu2()) {
-			window.getButtonPerso().setVisible(false);
-			window.getSizePanel().setVisible(false);
-			window.getPlayPerso().setEnabled(false);
-			window.getUp().setVisible(false);
-			window.getCenter().setVisible(false);
-			window.getDown().setVisible(false);
-			
-			if(gboard.getCells()!=null) {
-				gboard.removeListener();
-			}
-			
-			window.setSize(700,400);
-			window.getGameType().setVisible(true);
-		
+			simpleMenu();
 		}
 		
 		if(e.getSource()==window.getEasy()) {
@@ -141,25 +43,56 @@ public class btnListener implements ActionListener{
 		if(e.getSource()==window.getHard()) {
 			hardDifficulty();
 		}
-	}
-	
-	private void play(int value) {
-		boolean ended=false;
-		if(turn<=0) {
-			ended=turn(game.getRider(0),game.getRider(1),value);
-			if(ended) {
-				end(game.getRider(0));
-				return;
-			}
-		}else {
-			ended=turn(game.getRider(1),game.getRider(0),value);
-			if(ended) {
-				end(game.getRider(1));
-				return;
-			}
+		
+		if(e.getSource()==window.getRetourMenu()) {
+			returnMenu();
+		}
+		
+		if(e.getSource()==window.getPlay()) {
+			playGame();
+		}
+		
+		//Personalized game event
+		if(e.getSource()==window.getPerso()) {
+			persoMenu();
+		}
+		
+		if(e.getSource()==window.getBoxValidate()) {
+			validateBoardSize();
+		}
+		
+		if(e.getSource()==window.getRetourMenu2()) {
+			returnMenu2();
+		}
+		
+		if(e.getSource()==window.getPlayPerso()) {
+			playPersoGame();
+		}
+		
+		//General event
+		if(e.getSource()==window.getRoll()){
+			game.getDice().rouler();
+			int value=game.getDice().getValue();
+			gboard.getCells().get((int)(game.getBoard().getCols()/2)).get(3).setLabel("  "+value+"  ");
+			playTurn(value);
+		}
+		
+		if(e.getSource()==window.getStep()) {
+			int value=window.getStepValue();
+			window.getStep().setText("");
+			gboard.getCells().get((int)(game.getBoard().getCols()/2)).get(3).setLabel("  "+value+"  ");
+			playTurn(value);
 		}
 	}
 	
+	//Simple game functions
+	private void simpleMenu() {
+		window.getGameType().setVisible(false);
+		window.getDif().setVisible(true);
+		window.getButtonSimple().setVisible(true);
+		window.getC().add(window.getDif(),BorderLayout.CENTER);
+		window.getC().add(window.getButtonSimple(),BorderLayout.SOUTH);
+	}
 	
 	private void hardDifficulty() {
 		window.getPlay().setEnabled(true);
@@ -185,64 +118,106 @@ public class btnListener implements ActionListener{
 		gboard.setNbHedge(1);
 	}
 	
+	private void playGame(){
+		window.getButtonSimple().setVisible(false);
+		
+		window.getDif().setVisible(false);
+		initGame();
+		
+		window.getC().add(window.getUp(),BorderLayout.NORTH);
+		window.getC().add(window.getCenter(),BorderLayout.CENTER);
+		window.getC().add(window.getDown(),BorderLayout.SOUTH);
+		window.getUp().setVisible(true);
+		window.getCenter().setVisible(true);
+		window.getDown().setVisible(true);
+	}
+	
+	private void returnMenu() {
+		window.getButtonSimple().setVisible(false);
+		window.getDif().setVisible(false);
+		window.getPlay().setEnabled(false);
+		window.getGameType().setVisible(true);
+	}
+	
+	//Personalized game functions
+	private void persoMenu() {
+		game=null;
+		gboard.setNbHole(0);
+		gboard.setNbRiver(0);
+		gboard.setNbHedge(0);
+		
+		window.getGameType().setVisible(false);
+		window.getC().add(window.getSizePanel(),BorderLayout.NORTH);
+		sizeBoard=Integer.parseInt(window.getSelectBox().getSelectedItem().toString());
+		window.getSizePanel().setVisible(true);
+		window.getC().add(window.getButtonPerso(),BorderLayout.SOUTH);
+		window.getButtonPerso().setVisible(true);	
+	}
+	
+	private void validateBoardSize() {
+		sizeBoard=Integer.parseInt(window.getSelectBox().getSelectedItem().toString());
+		initGame();
+		window.getC().add(window.getCenter(),BorderLayout.CENTER);
+		window.getCenter().setVisible(true);
+		window.getPlayPerso().setEnabled(true);
+		gboard.addListener(game);
+	}
+	
+	private void playPersoGame() {
+		window.getButtonPerso().setVisible(false);
+		
+		window.getSizePanel().setVisible(false);
+		
+		window.getC().add(window.getUp(),BorderLayout.NORTH);
+		window.getC().add(window.getCenter(),BorderLayout.CENTER);
+		window.getC().add(window.getDown(),BorderLayout.SOUTH);
+		
+		window.getUp().setVisible(true);
+		window.getCenter().setVisible(true);
+		window.getDown().setVisible(true);
+	
+		gboard.removeListener();
+	}
+	
+	private void playTurn(int value) {
+		boolean ended=false;
+		if(turn<=0) {
+			ended=turn(game.getRider(0),game.getRider(1),value);
+			if(ended) {
+				end(game.getRider(0));
+				return;
+			}
+		}else {
+			ended=turn(game.getRider(1),game.getRider(0),value);
+			if(ended) {
+				end(game.getRider(1));
+				return;
+			}
+		}
+	}
+	
+	private void returnMenu2() {
+		window.getButtonPerso().setVisible(false);
+		window.getSizePanel().setVisible(false);
+		window.getPlayPerso().setEnabled(false);
+		window.getUp().setVisible(false);
+		window.getCenter().setVisible(false);
+		window.getDown().setVisible(false);
+		
+		if(gboard.getCells()!=null) {
+			gboard.removeListener();
+		}
+		
+		window.setSize(700,400);
+		window.getGameType().setVisible(true);
+	}
 	
 	
-	
+	//General functions
 	private void initGame() {
 		game=new Game(sizeBoard,false);
 		
 		gboard.init(game,window,sizeBoard);
-	}
-	
-	private boolean turn(Rider r1, Rider r2,int value){
-		String txtTurn="Le cavalier de couleur ";
-		txtTurn+=r1.getColor()==Couleur.RED?"ROUGE ":"BLEU ";
-		txtTurn+="joue\n";
-		window.getTxtTurn().setText(txtTurn);
-		char c=game.getBoard().getCellType(r1.getPos());
-		switch(c) {
-			case '@'://if hole : 
-				if(r1.inHole()==-1) {
-					go(r1,r2,value);
-				}else {
-					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
-				}
-				break;
-			case '|'://if hedge : 
-				if(value%2!=0) {
-					r1.setHedge(false);
-					go(r1,r2,value);
-				}else {
-					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
-				}
-				break;
-			case '~'://if river :
-				if(value%2==0) {
-					r1.setRiver(false);
-					go(r1,r2,value);
-				}else {
-					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
-				}
-				break;
-			case '='://if stable
-				if(value==6 && game.getBoard().getCellType(r1.getPos())=='=') {
-					go(r1,r2,1);
-				}else {
-					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
-				}
-				break;
-			case '*'://if end
-				return true;
-			default://if free :
-				go(r1,r2,value);
-				break;
-		}
-		c=game.getBoard().getCellType(r1.getPos());
-		if(c=='*') {
-			return true;
-		}
-		turn*=-1;
-		return false;
 	}
 
 	private void go(Rider r1, Rider r2, int de) {
@@ -301,16 +276,59 @@ public class btnListener implements ActionListener{
 		}
 	}
 	
-	
 	private void move(Rider r,Position from, Position to) {
 		gboard.getCells().get(from.getCol()).get(from.getRow()).setLabel("    ");
 		gboard.getCells().get(to.getCol()).get(to.getRow()).setLabel("  "+((r.getColor()==Couleur.RED)?"R":"B")+"  ");
 	}
 	
-	
-	/*Pour les bonus :
-	 * - Créer une zone de saisie pour le nombre d'obstacles
-	 * - Créer des boutons pour choisir la difficuté (un entier ? ou juste si source on initialise a telle valeur
-	 * - Créer un entier pour le nombre d'obstacle
-	 * */
+	private boolean turn(Rider r1, Rider r2,int value){
+		String txtTurn="Le cavalier de couleur ";
+		txtTurn+=r1.getColor()==Couleur.RED?"ROUGE ":"BLEU ";
+		txtTurn+="joue\n";
+		window.getTxtTurn().setText(txtTurn);
+		char c=game.getBoard().getCellType(r1.getPos());
+		switch(c) {
+			case '@'://if hole : 
+				if(r1.inHole()==-1) {
+					go(r1,r2,value);
+				}else {
+					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
+				}
+				break;
+			case '|'://if hedge : 
+				if(value%2!=0) {
+					r1.setHedge(false);
+					go(r1,r2,value);
+				}else {
+					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
+				}
+				break;
+			case '~'://if river :
+				if(value%2==0) {
+					r1.setRiver(false);
+					go(r1,r2,value);
+				}else {
+					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
+				}
+				break;
+			case '='://if stable
+				if(value==6 && game.getBoard().getCellType(r1.getPos())=='=') {
+					go(r1,r2,1);
+				}else {
+					window.getTxtProcess().setText(((CellPlayable)game.getBoard().getCell(r1.getPos())).process(r1));
+				}
+				break;
+			case '*'://if end
+				return true;
+			default://if free :
+				go(r1,r2,value);
+				break;
+		}
+		c=game.getBoard().getCellType(r1.getPos());
+		if(c=='*') {
+			return true;
+		}
+		turn*=-1;
+		return false;
+	}
 }
