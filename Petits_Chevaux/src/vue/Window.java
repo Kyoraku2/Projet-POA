f48a880209f348123b476abb7340b1a675161c4b
Almina <marie-almina.gindre@edu.univ-fcomte.fr>
2021-05-24 18:31:21 +0200

    C FE LES COM DE WINDOW C DE LA GROSS MERDE JE V ME YEET

--- Petits_Chevaux/src/vue/Window.java
+++ Petits_Chevaux/src/vue/Window.java
@@ -153,6 +153,11 @@ public class Window extends JFrame {
 	private JTextArea txtWinner;
 	
 
+	/**
+	 * Constructor of window,
+	 * Create all the elements of the game window
+	 * 
+	 */
 	public Window(){
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -163,7 +168,7 @@ public class Window extends JFrame {
 		c=this.getContentPane();		
 		c.setLayout(new BorderLayout());
 		
-///// Choix type de partie ////
+///// Game type menu ////
 		
 		pnlGameType=new JPanel();
 		pnlGameType.setLayout(new FlowLayout());
@@ -183,7 +188,7 @@ public class Window extends JFrame {
 		
 		c.add(pnlGameType);
 
-//// Parties simple ////
+//// Simple games ////
 		
 		pnlDif=new JPanel();
 		pnlDif.setBackground(Color.LIGHT_GRAY);
@@ -192,7 +197,7 @@ public class Window extends JFrame {
 		txtDifLab.setOpaque(true);
 		txtDifLab.setBackground(Color.LIGHT_GRAY);
 		
-		//Boutons de choix de la difficulté
+		//To chose the difficulty
 		btnEasy=new JButton("Facile");
 		btnEasy.addActionListener(list);
 		btnNormal=new JButton("Normal");
@@ -208,8 +213,10 @@ public class Window extends JFrame {
 		pnlBtnSimple=new JPanel();
 		pnlBtnSimple.setBackground(Color.LIGHT_GRAY);
 		
+		//to go back to the menu
 		btnBackSimple=new JButton("Retour au menu");
 		btnBackSimple.addActionListener(list);
+		//to play a game
 		btnPlaySimple=new JButton("Jouer");
 		btnPlaySimple.addActionListener(list);
 		btnPlaySimple.setEnabled(false);
@@ -221,28 +228,29 @@ public class Window extends JFrame {
 		
 		
 		
-//// Parties personnalisée ////
+//// Personalized games ////
 		
-		//Classe Graphic board 	
+		
 		pnlSizePerso=new JPanel();
 		pnlSizePerso.setBackground(Color.LIGHT_GRAY);
 		JTextArea TxtExplainPerso = new JTextArea("Veuillez choisir une taille pour votre plateau : \n\nAppuyer sur une case pour changer son type. ");
 		TxtExplainPerso.setBackground(Color.LIGHT_GRAY);
 		TxtExplainPerso.setEditable(false);
+		//to chose the size of the window
 		selectSize=new JComboBox<Integer>();
 		for(int i=10;i<32;++i) {
 			selectSize.addItem(i);
 		}
+		//to validate this size
 		btnBoxValidate=new JButton("Valider");
 		btnBoxValidate.addActionListener(list);
 		pnlSizePerso.add(TxtExplainPerso);
 		pnlSizePerso.add(selectSize);
 		pnlSizePerso.add(btnBoxValidate);
-		
-		
+		//to go back to the game type menu
 		btnBackPerso=new JButton("Retour au menu");
 		btnBackPerso.addActionListener(list);
-		
+		//to play a game
 		btnPlayPerso=new JButton("Jouer");
 		btnPlayPerso.addActionListener(list);
 		btnPlayPerso.setEnabled(false);
@@ -250,34 +258,31 @@ public class Window extends JFrame {
 		
 		pnlBtnPerso=new JPanel();
 		pnlBtnPerso.setBackground(Color.LIGHT_GRAY);
-	
 		pnlBtnPerso.setLayout(new FlowLayout());
 		
 		pnlBtnPerso.add(btnBackPerso);
 		pnlBtnPerso.add(btnPlayPerso);
 
 		
-//// Fenetre de jeu ////
+//// Game  ////
 		
-	// HAUT 
+	// UP
 		pnlPlayUp.setBackground(Color.LIGHT_GRAY);
 		pnlPlayUp.setSize(this.getWidth(),100);
 		pnlPlayUp.setLayout(new GridLayout(2,1));
 		
-		//Text indiquant le joueur du tour
+		//text to know with player played and what he did
 		txtTurn=new JTextField();
 		txtTurn.setEditable(false);
 		txtTurn.setBackground(Color.LIGHT_GRAY);
 		txtTurn.setBorder(null);
 		txtTurn.setHorizontalAlignment(JTextField.CENTER);
 		
-		//partie des boutons
 		pnlPlayUpBtn.setBackground(Color.LIGHT_GRAY);	
-		
+		//to roll a dice
 		btnRoll=new JButton("Rouler le dé");
 		btnRoll.addActionListener(list);
-
-		//Choix de valeur du dé par utilisateur
+		//To chose the value instead
 		JLabel txtStepAsk =new JLabel("        Saisir valeur Dé :");
 		txtStep=new JTextField(3);
 		txtStep.addActionListener(list);	
@@ -290,15 +295,15 @@ public class Window extends JFrame {
 		pnlPlayUp.add(pnlPlayUpBtn);
 		
 		
-	// CENTRE
+	// CENTER
 		
-		// Dans listener
+		// All the element of the center pannel are created and updated in the listener, GraphicCell or GraphicBoard
 		
-	// BAS
+	// DOWN
 		pnlPlayDown.setBackground(Color.LIGHT_GRAY);
 		pnlPlayUp.setSize(this.getWidth(),100);
 		
-		//texte de process
+		//text of the process functions
 		txtProcess=new JTextArea(2, 1);
 		txtProcess.setEditable(false);
 		txtProcess.setBorder(null);
@@ -306,19 +311,19 @@ public class Window extends JFrame {
 
 		pnlPlayDown.add(txtProcess);
 		
-///// MENU FIN ////
+///// END MENU ////
 		
 		pnlEndMenu=new JPanel();
 		pnlEndMenu.setLayout(new FlowLayout());
 		pnlEndMenu.setBackground(Color.LIGHT_GRAY);
-		
 		pnlEndMenu.setLayout(new BorderLayout());
-		
+		// To replay
 		btnReplay = new JButton("Rejouer");
 		btnReplay.addActionListener(list);
+		// To quit
 		btnQuit = new JButton("Quitter");
 		btnQuit.addActionListener(list);
-		
+		// Text to know who is the winner
 		txtWinner = new JTextArea(2,1);
 		txtWinner.setEditable(false);
 		txtWinner.setBorder(null);
@@ -327,14 +332,12 @@ public class Window extends JFrame {
 		JPanel pnlEndMenuBtn = new JPanel();
 		pnlEndMenuBtn.setLayout(new FlowLayout());
 		pnlEndMenuBtn.setBackground(Color.LIGHT_GRAY);;
-		
 		pnlEndMenuBtn.add(btnReplay);
 		pnlEndMenuBtn.add(btnQuit);
 		
 		JPanel pnlWinner = new JPanel();
 		pnlWinner.setBackground(Color.LIGHT_GRAY);
 		pnlWinner.setSize(this.getWidth(),100);
-		
 		pnlWinner.add(txtWinner);
 		
 		pnlEndMenu.add(pnlWinner,BorderLayout.NORTH);
@@ -342,106 +345,211 @@ public class Window extends JFrame {
 		
 	}
 	
-	
+	/**
+	 * Getter of the button allowing to quit when the party is finished
+	 * 
+	 * @return The quit button
+	 */
 	public JButton getBtnQuit() {
 		return btnQuit;
 	}
-	
+	/**
+	 * Getter of the text showing the winner
+	 * 
+	 * @return The text of the end menu
+	 */
 	public JTextArea getTxtWinner() {
 		return txtWinner;
 	}
-	
+	/**
+	 * Getter of the panel where all the button from the simple game menu are
+	 * 
+	 * @return The panel containing the simple menu's buttons
+	 */
 	public JPanel getPnlSimpleBtn() {
 		return pnlBtnSimple;
 	}
+	/**
+	 * Getter of the panel where all the button from the perso game menu are
+	 * 
+	 * @return The panel containing the perso menu's buttons
+	 */
 	public JPanel getPnlPersoBtn() {
 		return pnlBtnPerso;
 	}
-	
+	/**
+	 * Getter of the panel of the end menu
+	 * 
+	 * @return The panel of the end menu
+	 */
 	public JPanel getEndMenu() {
 		return pnlEndMenu;
 	}
-	
+	/**
+	 * Getter of the button allowing to go back from the simple menu to the game type one
+	 * 
+	 * @return The return button of simple
+	 */
 	public JButton getBtnBackSimple() {
 		return btnBackSimple;
 	}
-	
+	/**
+	 * Getter of the button allowing to go back from the perso menu to the game type one
+	 * 
+	 * @return The return button of perso
+	 */
 	public JButton getBtnBackPerso() {
 		return btnBackPerso;
 	}
-	
+	/**
+	 * Getter of the button to validate the length of the board in personalized games
+	 * 
+	 * @return The validate button 
+	 */
 	public JButton getBoxValidate() {
 		return btnBoxValidate;
 	}
-	
+	/**
+	 * Getter of the button to validate the length of the board in personalized games
+	 * 
+	 * @return The validate button 
+	 */
 	public JPanel getSizePanel() {
 		return pnlSizePerso;
 	}
-	
+	/**
+	 * Getter of the select box allowing to chose the size of the board
+	 * 
+	 * @return The select box
+	 */
 	public JComboBox<Integer> getSelectBox() {
 		return selectSize;
 	}
-	
+	/**
+	 * Getter of the panel containing the menu to chose the type of the game
+	 * 
+	 * @return The panel of the game type menu
+	 */
 	public JPanel getPnlGameType() {
 		return pnlGameType;
 	}
-	
+	/**
+	 * Getter of the button to chose the simple game
+	 * 
+	 * @return The simple game button
+	 */
 	public JButton getBtnSimple() {
 		return btnSimpleGame;
 	}
-	
+	/**
+	 * Getter of the button to chose the personalized game
+	 * 
+	 * @return The personalized game button
+	 */
 	public JButton getBtnPerso() {
 		return btnPersoGame;
 	}
-	
+	/**
+	 * Getter of the panel containing the difficulty buttons
+	 * 
+	 * @return The difficulty panel
+	 */
 	public JPanel getPnlDif() {
 		return pnlDif;
 	}
-	
+	/**
+	 * Getter of the button to play a personalized game
+	 * 
+	 * @return The play button
+	 */
 	public JButton getBtnPlayPerso() {
 		return btnPlayPerso;
 	}
-	
+	/**
+	 * Getter of the button to play a simple game
+	 * 
+	 * @return The play button
+	 */
 	public JButton getBtnPlaySimple() {
 		return btnPlaySimple;
 	}
-	
+	/**
+	 * Getter of the button to set the easy difficulty
+	 * 
+	 * @return The difficulty button
+	 */
 	public JButton getBtnEasy() {
 		return btnEasy;
 	}
-	
+	/**
+	 * Getter of the button to set the normal difficulty
+	 * 
+	 * @return The difficulty button
+	 */
 	public JButton getBtnNormal() {
 		return btnNormal;
 	}
-	
+	/**
+	 * Getter of the button to set the hard difficulty
+	 * 
+	 * @return The difficulty button
+	 */
 	public JButton getBtnHard() {
 		return btnHard;
 	}
-	
+	/**
+	 * Getter of the container of the window
+	 * 
+	 * @return The container of the window
+	 */
 	public Container getC() {
 		return c;
 	}
-	
+	/**
+	 * Getter of the upper panel of the game window
+	 * 
+	 * @return The panel
+	 */
 	public JPanel getPnlPlayUp() {
 		return pnlPlayUp;
 	}
-	
+	/**
+	 * Getter of the center panel of the game window
+	 * 
+	 * @return The panel
+	 */
 	public JPanel getPnlPlayCenter() {
 		return pnlPlayCenter;
 	}
-	
+	/**
+	 * Getter of the down panel of the game window
+	 * 
+	 * @return The panel
+	 */
 	public JPanel getPnlPlayDown() {
 		return pnlPlayDown;
 	}
-	
+	/**
+	 * Getter of the upper panel of the game window
+	 * 
+	 * @return The panel
+	 */
 	public JButton getBtnRoll() {
 		return btnRoll;
 	}
-	
+	/**
+	 * Getter of the text field to chose the value of the dice
+	 * 
+	 * @return The text field
+	 */
 	public JTextField getTxtStep() {
 		return txtStep;
 	}
-	
+	/**
+	 * Getter of the chosen value of the dice
+	 * 
+	 * @return The chosen value
+	 */
 	public int getStepValue() {
 		int res;
 		try {
@@ -452,15 +560,27 @@ public class Window extends JFrame {
 		}
 		return res;
 	}
-	
+	/**
+	 * Getter of the text field that shows which player played
+	 * 
+	 * @return The text field
+	 */
 	public JTextField getTxtTurn() {
 		return txtTurn;
 	}
-
+	/**
+	 * Getter of the text field used to show the return of the process functions
+	 * 
+	 * @return The text field
+	 */
 	public JTextArea getTxtProcess() {
 		return txtProcess;
 	}
-	
+	/**
+	 * Getter of the button to replay when a game is finished
+	 * 
+	 * @return The replay button
+	 */
 	public JButton getBtnReplay() {
 		return btnReplay;
 	}
